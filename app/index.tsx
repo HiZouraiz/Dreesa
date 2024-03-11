06277abd975ee22d107a549b958e9f88ae6bd5e1
HiZouraiz <officialzouraiz@gmail.com>
2024-03-11 21:48:59 +0500

    Initial Commit.

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl text-black-800 p-4">
+        React native expo with Router V2, Tailwind Css, TypeScript
+      </Text>
+    </View>
+  );
+}
